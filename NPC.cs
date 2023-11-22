c68583f9ed3610d993cd49e8e3c8b95d22b778f2
vasailije <150952151+vasailije@users.noreply.github.com>
2023-11-22 19:00:55 +0100

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // NPC Health
+    public int health = 5;
+    //NPC Level
+    public int level = 1;
+
+    //NPC Speed
+    public float speed = 1.2f;
+
+    void Start()
+    {
+
+    }
+     ... 
+}
